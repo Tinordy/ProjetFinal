9479471c4c8a7cb4a293f7941f6d37d78ff2f210
david <david@DESKTOP-5G5AQFQ>
2017-03-27 18:51:53 -0400

    les maisons affichent pu...

--- Jeu/Jeu/Section.cs
+++ Jeu/Jeu/Section.cs
@@ -12,45 +12,54 @@ using Microsoft.Xna.Framework.Media;
 
 namespace AtelierXNA
 {
-    /// <summary>
-    /// This is a game component that implements IUpdateable.
-    /// </summary>
-    public class Section : Terrain
-    {
-        const float INTERVALLE_MAJ_STANDARD = 1f / 60f;
-        Vector2 Étendue { get; set; }
-        Vector2 Extrémité { get; set; }
-        double NormeÉtendue { get; set; }
-        Maison Test { get; set; }
-        
-        List<GameComponent> Components { get; set; }
-        public Section(Game game, Vector2 origine, Vector2 étendue2,float homothétieInitiale, Vector3 rotationInitiale, Vector3 positionInitiale, Vector3 étendue,
-                       string[] nomsTexturesTerrain, float intervalleMAJ)
-            : base(game, origine,homothétieInitiale, rotationInitiale, positionInitiale, étendue, nomsTexturesTerrain, intervalleMAJ)
-        {
-            //Étendue = étendue2;
-            Étendue = new Vector2(étendue.X, étendue.Z);
+   /// <summary>
+   /// This is a game component that implements IUpdateable.
+   /// </summary>
+   public class Section : Terrain
+   {
+      const float INTERVALLE_MAJ_STANDARD = 1f / 60f;
+      Vector2 Étendue { get; set; }
+      Vector2 Extrémité { get; set; }
+      double NormeÉtendue { get; set; }
+      Maison Test { get; set; }
 
-        }
+      List<DrawableGameComponent> Components { get; set; }
+      public Section(Game game, Vector2 origine, Vector2 étendue2, float homothétieInitiale, Vector3 rotationInitiale, Vector3 positionInitiale, Vector3 étendue,
+                     string[] nomsTexturesTerrain, float intervalleMAJ)
+          : base(game, origine, homothétieInitiale, rotationInitiale, positionInitiale, étendue, nomsTexturesTerrain, intervalleMAJ)
+      {
+         //Étendue = étendue2;
+         Étendue = new Vector2(étendue.X, étendue.Z);
 
-        public override void Initialize()
-        {
-            Extrémité = Coin + Étendue;
-            Test = new Maison(Game, 10f, Vector3.Zero, new Vector3(Coin.X/* + Étendue.X*/, 0, Coin.Y/* - Étendue.Y*/), new Vector3(2,2,2), "brique1", "roof", 0.01f);
-            Game.Components.Add(Test);
-            NormeÉtendue = Math.Sqrt(Math.Pow(Étendue.X, 2) + Math.Pow(Étendue.Y, 2));
-            CréerPiste();
-            base.Initialize();
-        }
-        //lol
-        private void CréerPiste()
-        {
-            //Components.Add(new PisteSectionnée(Game, 1f, Vector3.Zero, Vector3.Zero, INTERVALLE_MAJ_STANDARD, 20000, 20000, Coin, Étendue));
-        }
+      }
 
-        //public void AddComponent(GameComponent x)
-        //{
-        //    Components.Add(x);
-        //}
-    }
+      public override void Initialize()
+      {
+         Components = new List<DrawableGameComponent>();
+         Extrémité = Coin + Étendue;
+         Test = new Maison(Game, 10f, Vector3.Zero, new Vector3(Coin.X/* + Étendue.X*/, 0, Coin.Y/* - Étendue.Y*/), new Vector3(2, 2, 2), "brique1", "roof", 0.01f);
+         Game.Components.Add(Test);
+         Components.Add(Test);
+         NormeÉtendue = Math.Sqrt(Math.Pow(Étendue.X, 2) + Math.Pow(Étendue.Y, 2));
+         CréerPiste();
+         base.Initialize();
+      }
+      //lol
+      private void CréerPiste()
+      {
+         //Components.Add(new PisteSectionnée(Game, 1f, Vector3.Zero, Vector3.Zero, INTERVALLE_MAJ_STANDARD, 20000, 20000, Coin, Étendue));
+      }
+      protected override void OnVisibleChanged(object sender, EventArgs args)
+      {
+         foreach(DrawableGameComponent c in Components)
+         {
+            c.Visible = Visible;
+         }
+      }
+
+      //public void AddComponent(GameComponent x)
+      //{
+      //    Components.Add(x);
+      //}
+   }
 }
