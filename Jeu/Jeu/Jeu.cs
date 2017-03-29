1be20fe2e1728c6d7f9fef948bb04e8ce8bf2eaf
david <david@DESKTOP-5G5AQFQ>
2017-03-29 09:41:40 -0400

    pause ennemi

--- Jeu/Jeu/Jeu.cs
+++ Jeu/Jeu/Jeu.cs
@@ -148,6 +148,7 @@ namespace AtelierXNA
             if(NetworkManager.EnnemiPrêtÀJouer)
             {
                 État = ÉtatsJeu.JEU;
+                Pause = false;
             }
         }
 
