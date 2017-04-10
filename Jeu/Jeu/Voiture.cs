10de5e4fa53e421d8c71aa88b07ea9d99c41aa28
David Choquet <david.choquet44@gmail.com>
2017-04-10 10:14:57 -0400

    try

--- Jeu/Jeu/Voiture.cs
+++ Jeu/Jeu/Voiture.cs
@@ -129,7 +129,7 @@ namespace AtelierXNA
             Direction = new Vector3(0, 0, 75);
             Vitesse = 0;
             base.Initialize();
-            SphèreDeCollision = new BoundingSphere(Position, Norme(Monde.Forward, Monde.Backward));
+            SphèreDeCollision = new BoundingSphere(Position, Norme(Monde.Forward, Monde.Backward)*Échelle);
             DirectionCaméra = Monde.Forward - Monde.Backward;
             DéplacerCaméra();
         }
