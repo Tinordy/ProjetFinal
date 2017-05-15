450ced02ebeee506c21a32625be7819b4613ccc3
David Choquet <david.choquet44@gmail.com>
2017-05-15 08:04:22 -0400

    push serveur

--- Jeu/Jeu/Serveur.cs
+++ Jeu/Jeu/Serveur.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Net.Sockets;
 using System.Net;
 using System.IO;
-using System.Threading;
 using Microsoft.Xna.Framework;
-using System.Windows.Forms;
 
 namespace AtelierXNA
 {
